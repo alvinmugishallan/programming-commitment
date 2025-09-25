d08ac4d71e04f41b4228fe0807eda69b4589328a
alvinmugishallan <alvinmugisha06@gmail.com>
2025-09-25 11:29:15 +0300

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,41 @@
+// let coursework=35
+// let exam=30
+// let finalmark=coursework+exam;
+// if (finalmark<=49){
+//     console.log("Grade F");
+// }else if (finalmark<=59) {
+//     console.log("Grade C");
+// }
+// else if (finalmark<=69) {
+//     console.log("Grade B");
+// }else if (finalmark<=100) {
+//     console.log("Grade A");
+// };
+
+let weekdDayNumber=4;
+switch (weekdDayNumber) {
+    case 1:
+        console.log("Monday");  
+        break
+    case 2:
+        console.log("Tuesday");
+        break
+    case 3:
+        console.log("Wednesday");
+        break   
+    case 4:
+        console.log("Thursday");
+        break    
+    case 5:
+        console.log("Friday");  
+        break
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7 :
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day number");
+        break;
+}
